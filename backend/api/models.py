59e73ba814af006d4b9208b7d7c459edb96bac59
Marek <m.rykaczewski99@gmail.com>
2024-05-28 15:36:21 +0200

    Revert "add custom user model"
    This reverts commit 4259221e5123b97b8278d9f844e4b74a90930737.

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,5 +1,3 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 
-class User(AbstractUser):
-    email = models.EmailField(unique=True, blank=False, null=False)
+# Create your models here.
