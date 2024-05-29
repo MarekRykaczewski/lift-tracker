faf77a56d74f8d8f51ef472ab544756be0744d56
Marek <m.rykaczewski99@gmail.com>
2024-05-29 16:51:45 +0200

    add custom user

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,12 +1,21 @@
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
+
+User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "username", "password"]
-        extra_kwargs = {"password": {"write_only": True}}
+        fields = ["id", "username", "email", "password"]
+        extra_kwargs = {
+            "password": {"write_only": True},
+            "email": {"required": True}
+        }
 
-        def create(self, validated_data):
-            user = User.objects.create_user(**validated_data)
-            return user
+    def create(self, validated_data):
+        password = validated_data.pop("password", None)
+        user = User(**validated_data)
+        if password:
+            user.set_password(password)
+        user.save()
+        return user
--- backend/api/views.py
+++ backend/api/views.py
@@ -3,8 +3,10 @@ from django.contrib.auth.models import User
 from rest_framework import generics
 from .serializers import UserSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+User = get_user_model()
+
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
