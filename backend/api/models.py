34460df10da5b487e091517dd63ca24437a95e63
Marek <m.rykaczewski99@gmail.com>
2024-05-30 14:05:17 +0200

    add new models

--- backend/api/models.py
+++ backend/api/models.py
@@ -4,3 +4,26 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     email = models.EmailField(unique=True, blank=False, null=False)
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, related_name="profile", on_delete=models.CASCADE)
+    weight = models.FloatField(null=True, blank=True)
+    body_fat_percentage = models.FloatField(null=True, blank=True)
+
+class Workout(models.Model):
+    date = models.DateField()
+    notes = models.TextField(blank=True, null=True)
+
+class Exercise(models.Model):
+    name = models.CharField(max_length=100)
+
+class Set(models.Model):
+    workout = models.ForeignKey(Workout, related_name="sets", on_delete=models.CASCADE)
+    exercise = models.ForeignKey(Exercise, related_name="sets", on_delete=models.CASCADE)
+    order = models.PositiveIntegerField()
+    reps = models.PositiveIntegerField()
+    weight = models.FloatField()
+
+    class Meta:
+        unique_together = ('workout', 'exercise', 'order')
+        ordering = ['order']
