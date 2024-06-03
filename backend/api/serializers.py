5c6c553c843451b779801d42f2c7c8e8760890f9
Marek <m.rykaczewski99@gmail.com>
2024-06-03 17:03:52 +0200

    add serializers

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from .models import UserProfile, Exercise, Set, Workout
 
 User = get_user_model()
 
@@ -19,3 +20,32 @@ class UserSerializer(serializers.ModelSerializer):
             user.set_password(password)
         user.save()
         return user
+    
+class UserProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = UserProfile
+        fields = ['id', 'user', 'weight', 'body_fat_percentage']
+    
+class ExerciseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Exercise
+        fields = ['id', 'name']
+
+
+class SetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Set
+        fields = ['id', 'workout', 'exercise', 'order', 'reps', 'weight']
+        extra_kwargs = {
+            'workout': {'read_only': True},
+            'exercise': {'read_only': True}
+        }
+
+class WorkoutSerializer(serializers.ModelSerializer):
+    sets = SetSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Workout
+        fields = ['id', 'date', 'notes', 'sets']
