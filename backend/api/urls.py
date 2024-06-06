7bb9748357e5d2cf90372d11748691470f4743e3
Marek <m.rykaczewski99@gmail.com>
2024-06-06 10:59:46 +0200

    add workouts/{date}

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import WorkoutListCreateView
+from .views import WorkoutListCreateView, WorkoutDetailView
 
 urlpatterns = [
     path('workouts/', WorkoutListCreateView.as_view(), name='workout-list-create'),
+    path('workouts/<str:date>/', WorkoutDetailView.as_view(), name='workout-detail-view'),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
-from .serializers import UserSerializer, UserProfileSerializer, WorkoutSerializer, ExerciseSerializer, SetSerializer
-from .models import User, UserProfile, Workout, Exercise, Set
+from .serializers import UserSerializer, WorkoutSerializer
+from .models import User, Workout
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from django.contrib.auth import get_user_model
+from django.http import Http404
 
 User = get_user_model()
 
@@ -26,3 +26,23 @@ class WorkoutListCreateView(generics.ListCreateAPIView):
             serializer.save(user=self.request.user)
         else:
             print(serializer.errors)
+
+class WorkoutDetailView(generics.RetrieveAPIView):
+    queryset = Workout.objects.all()
+    serializer_class = WorkoutSerializer
+    permission_classes = [IsAuthenticated]
+    lookup_field = 'date' 
+
+    def get_object(self):
+        queryset = self.filter_queryset(self.get_queryset())
+        filter_kwargs = {
+            self.lookup_field: self.kwargs[self.lookup_field],
+            'user': self.request.user
+        }
+        try:
+            obj = queryset.get(**filter_kwargs)
+        except Workout.MultipleObjectsReturned:
+            obj = queryset.filter(**filter_kwargs).first()
+        except Workout.DoesNotExist:
+            raise Http404("Workout not found")
+        return obj
