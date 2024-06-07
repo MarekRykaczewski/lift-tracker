1be8a19a49c0143938f2439713ac0b699db0cf4c
Marek <m.rykaczewski99@gmail.com>
2024-06-07 16:44:09 +0200

    add SetGroup view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework import generics
-from .serializers import UserSerializer, WorkoutSerializer, SetSerializer, ExerciseSerializer
-from .models import User, Workout, Set, Exercise
+from .serializers import UserSerializer, WorkoutSerializer, SetSerializer, SetGroupSerializer, ExerciseSerializer
+from .models import User, Workout, Set, SetGroup, Exercise
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from django.contrib.auth import get_user_model
 from django.http import Http404
@@ -47,18 +47,18 @@ class WorkoutDetailView(generics.RetrieveAPIView):
             raise Http404("Workout not found")
         return obj
     
-class SetListCreateView(generics.ListCreateAPIView):
-    queryset = Set.objects.all()
-    serializer_class = SetSerializer
+class SetGroupListCreateView(generics.ListCreateAPIView):
+    queryset = SetGroup.objects.all()
+    serializer_class = SetGroupSerializer
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         date = self.kwargs['date']
         try:
             workout = Workout.objects.get(date=date, user=self.request.user)
-            return Set.objects.filter(workout=workout)
+            return SetGroup.objects.filter(workout=workout)
         except Workout.DoesNotExist:
-            return Set.objects.none()
+            return SetGroup.objects.none()
 
     def perform_create(self, serializer):
         date = self.kwargs['date']
@@ -67,6 +67,27 @@ class SetListCreateView(generics.ListCreateAPIView):
             serializer.save(workout=workout)
         except Workout.DoesNotExist:
             raise Http404("Workout not found")
+    
+class SetListCreateView(generics.ListCreateAPIView):
+    queryset = Set.objects.all()
+    serializer_class = SetSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        set_group_id = self.kwargs['set_group_id']
+        try:
+            set_group = SetGroup.objects.get(id=set_group_id, workout__user=self.request.user)
+            return Set.objects.filter(set_group=set_group)
+        except SetGroup.DoesNotExist:
+            return Set.objects.none()
+
+    def perform_create(self, serializer):
+        set_group_id = self.kwargs['set_group_id']
+        try:
+            set_group = SetGroup.objects.get(id=set_group_id, workout__user=self.request.user)
+            serializer.save(set_group=set_group)
+        except SetGroup.DoesNotExist:
+            raise Http404("SetGroup not found")
         
 class ExerciseListView(generics.ListAPIView):
     queryset = Exercise.objects.all()
