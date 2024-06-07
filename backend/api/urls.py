0b54b8f0222b60a2d941e66d66e278882872be4c
Marek <m.rykaczewski99@gmail.com>
2024-06-07 15:37:17 +0200

    add new views to urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import WorkoutListCreateView, WorkoutDetailView
+from .views import WorkoutListCreateView, WorkoutDetailView, SetListCreateView, ExerciseListView
 
 urlpatterns = [
     path('workouts/', WorkoutListCreateView.as_view(), name='workout-list-create'),
     path('workouts/<str:date>/', WorkoutDetailView.as_view(), name='workout-detail-view'),
+    path('workouts/<str:date>/sets/', SetListCreateView.as_view(), name='set-create'),
+    path('exercises/', ExerciseListView.as_view(), name='exercise-list')
 ]
