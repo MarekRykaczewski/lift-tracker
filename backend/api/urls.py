41a49ca824d39ebb8320605b7823c4a91d2c911d
Marek <m.rykaczewski99@gmail.com>
2024-06-07 16:46:15 +0200

    add setgroup url

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import WorkoutListCreateView, WorkoutDetailView, SetListCreateView, ExerciseListView
+from .views import WorkoutListCreateView, WorkoutDetailView, SetListCreateView, SetGroupListCreateView, ExerciseListView
 
 urlpatterns = [
     path('workouts/', WorkoutListCreateView.as_view(), name='workout-list-create'),
     path('workouts/<str:date>/', WorkoutDetailView.as_view(), name='workout-detail-view'),
-    path('workouts/<str:date>/sets/', SetListCreateView.as_view(), name='set-create'),
-    path('exercises/', ExerciseListView.as_view(), name='exercise-list')
+    path('workouts/<str:date>/set-groups/', SetGroupListCreateView.as_view(), name='setgroup-list-create'),
+    path('workouts/set-groups/<int:set_group_id>/sets/', SetListCreateView.as_view(), name='set-list-create'),
+    path('exercises/', ExerciseListView.as_view(), name='exercise-list'),
 ]
