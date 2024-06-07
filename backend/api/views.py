1a185577520d0ae5d179b842e1060bc364ef04a2
Marek <m.rykaczewski99@gmail.com>
2024-06-07 15:36:49 +0200

    add SetListCreateView

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework import generics
-from .serializers import UserSerializer, WorkoutSerializer
-from .models import User, Workout
+from .serializers import UserSerializer, WorkoutSerializer, SetSerializer, ExerciseSerializer
+from .models import User, Workout, Set, Exercise
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from django.contrib.auth import get_user_model
 from django.http import Http404
@@ -46,3 +46,24 @@ class WorkoutDetailView(generics.RetrieveAPIView):
         except Workout.DoesNotExist:
             raise Http404("Workout not found")
         return obj
+    
+class SetListCreateView(generics.ListCreateAPIView):
+    queryset = Set.objects.all()
+    serializer_class = SetSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        date = self.kwargs['date']
+        try:
+            workout = Workout.objects.get(date=date, user=self.request.user)
+            return Set.objects.filter(workout=workout)
+        except Workout.DoesNotExist:
+            return Set.objects.none()
+
+    def perform_create(self, serializer):
+        date = self.kwargs['date']
+        try:
+            workout = Workout.objects.get(date=date, user=self.request.user)
+            serializer.save(workout=workout)
+        except Workout.DoesNotExist:
+            raise Http404("Workout not found")
