6f0446a018d06a6f0de68b8c4cecef75bf3efdfa
Marek <m.rykaczewski99@gmail.com>
2024-06-10 17:15:34 +0200

    replace space with margin right

--- frontend/src/components/Set.tsx
+++ frontend/src/components/Set.tsx
@@ -1,12 +1,23 @@
-const Set = ({ set }) => {
+import { useState } from "react";
+
+const Set = ({ set, onSave }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [weight, setWeight] = useState(set.weight);
+  const [reps, setReps] = useState(set.reps);
+
+  const handleSave = () => {
+    onSave(set.id, weight, reps);
+    setIsEditing(false);
+  };
+
   return (
     <div className="flex justify-between p-1" key={set.id}>
       <div>
-        <span className="font-bold">{set.weight}</span>{" "}
+        <span className="font-bold mr-1">{set.weight}</span>
         <span className="text-gray-700">lbs</span>
       </div>
       <div>
-        <span className="font-bold">{set.reps}</span>{" "}
+        <span className="font-bold mr-1">{set.reps}</span>
         <span className="text-gray-700">reps</span>
       </div>
     </div>
