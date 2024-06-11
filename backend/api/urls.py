acbb2eeecdd66ed11afb859e945e292919db9f79
Marek <m.rykaczewski99@gmail.com>
2024-06-11 16:59:10 +0200

    add url for set destroy / update view

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import WorkoutListCreateView, WorkoutDetailView, SetListCreateView, SetGroupListCreateView, ExerciseListView
+from .views import WorkoutListCreateView, WorkoutDetailView, SetListCreateView, SetGroupListCreateView, ExerciseListView, SetRetrieveUpdateDestroyView
 
 urlpatterns = [
     path('workouts/', WorkoutListCreateView.as_view(), name='workout-list-create'),
     path('workouts/<str:date>/', WorkoutDetailView.as_view(), name='workout-detail-view'),
     path('workouts/<str:date>/set-groups/', SetGroupListCreateView.as_view(), name='setgroup-list-create'),
     path('workouts/set-groups/<int:set_group_id>/sets/', SetListCreateView.as_view(), name='set-list-create'),
+    path('workouts/set-groups/<int:set_group_id>/sets/<int:pk>/', SetRetrieveUpdateDestroyView.as_view(), name='set-retrieve-update-destroy'),
     path('exercises/', ExerciseListView.as_view(), name='exercise-list'),
 ]
