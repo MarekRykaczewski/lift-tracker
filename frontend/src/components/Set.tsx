611943bb76837b97c78c2ebc8ebb1a17e17992ea
Marek <m.rykaczewski99@gmail.com>
2024-06-14 14:38:17 +0200

    styling changes

--- frontend/src/components/Set.tsx
+++ frontend/src/components/Set.tsx
@@ -2,9 +2,12 @@ const Set = ({ set, isSelected }) => {
   return (
     <div
       className={`flex justify-between p-1 border-b ${
-        isSelected ? "bg-gray-200" : ""
+        isSelected ? "bg-sky-100" : ""
       }`}
     >
+      <div>
+        <span className="font-bold mr-1">{set.order}</span>
+      </div>
       <div>
         <span className="font-bold mr-1">{set.weight}</span>
         <span className="text-gray-700">lbs</span>
--- frontend/src/pages/SetGroupDetails.tsx
+++ frontend/src/pages/SetGroupDetails.tsx
@@ -102,40 +102,43 @@ const SetGroupDetails = () => {
       <h1 className="w-full font-bold text-2xl text-center py-3 mb-3 bg-gray-900 text-white border-b-4 border-sky-500">
         Track
       </h1>
-      <div className="max-w-[50vw] border-b flex flex-col">
+      <div className="max-w-[40vw] w-full border-b flex flex-col">
         <form
-          className="flex flex-col"
+          className="flex flex-col mb-2"
           onSubmit={selectedSet ? handleUpdateSet : handleCreateSet}
         >
-          <label className="border-b-2 font-semibold border-sky-500 px-3">
-            Weight
+          <label className="border-b-2 mb-1 font-semibold border-sky-500 ">
+            WEIGHT
           </label>
           <input
-            className="border"
+            className="border-b self-center font-bold text-xl border-gray-400 text-center w-1/3 bg-transparent"
             type="number"
             value={formState.weight}
             onChange={(e) =>
               setFormState({ ...formState, weight: e.target.value })
             }
           />
-          <label className="border-b-2 font-semibold border-sky-500 px-3">
-            Reps
+          <label className="border-b-2 mb-1 font-semibold border-sky-500">
+            REPS
           </label>
           <input
-            className="border"
+            className="border-b self-center font-bold text-xl border-gray-400 text-center w-1/3 bg-transparent"
             type="number"
             value={formState.reps}
             onChange={(e) =>
               setFormState({ ...formState, reps: e.target.value })
             }
           />
-          <div className="flex">
-            <button className="border bg-white p-2" type="submit">
+          <div className="flex justify-center gap-2 mt-2">
+            <button
+              className="rounded text-white font-bold px-4 py-2 bg-blue-500"
+              type="submit"
+            >
               {selectedSet ? "Update" : "Add"}
             </button>
             {!selectedSet && (
               <button
-                className="border bg-white p-2"
+                className="rounded text-white font-bold px-4 py-2 bg-gray-500"
                 type="button"
                 onClick={() => {
                   setSelectedSet(null);
@@ -147,7 +150,7 @@ const SetGroupDetails = () => {
             )}
             {selectedSet && (
               <button
-                className="border bg-white p-2"
+                className="rounded text-white font-bold px-4 py-2 bg-red-500"
                 type="button"
                 onClick={handleDeleteSet}
               >
@@ -157,7 +160,7 @@ const SetGroupDetails = () => {
           </div>
         </form>
         {error && <div className="text-red-500">{error}</div>}
-        <div className="ml-[50%] px-3">
+        <div>
           {sets.map((set) => (
             <div key={set.id} onClick={() => handleSetClick(set)}>
               <Set set={set} isSelected={selectedSet?.id === set.id} />
