4828673c89226900f7ccd5ae3245675f0915b6ef
Marek <m.rykaczewski99@gmail.com>
2024-06-14 14:51:20 +0200

    remove header

--- frontend/src/pages/Workouts.tsx
+++ frontend/src/pages/Workouts.tsx
@@ -3,7 +3,6 @@ import YearCalendar from "../components/YearCalendar";
 const Workouts = () => {
   return (
     <div className="w-full">
-      <h1>Workouts</h1>
       <YearCalendar />
     </div>
   );
