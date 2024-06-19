22988999a89a324e51fb040078d021a5af6d58d4
Marek <m.rykaczewski99@gmail.com>
2024-06-19 19:31:06 +0200

    style changes

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,7 +1,11 @@
 import AuthForm from "../components/AuthForm";
 
 const Login = () => {
-  return <AuthForm route="/api/token/" method="login" />;
+  return (
+    <div className="w-full h-full p-3 mt-auto mb-auto">
+      <AuthForm route="/api/token/" method="login" />;
+    </div>
+  );
 };
 
 export default Login;
