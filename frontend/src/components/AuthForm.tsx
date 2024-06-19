6e70191dd916c051b848d21b171a19d811f3d2a7
Marek <m.rykaczewski99@gmail.com>
2024-06-19 17:50:34 +0200

    expand AuthForm structure and styles

--- frontend/src/components/AuthForm.tsx
+++ frontend/src/components/AuthForm.tsx
@@ -43,35 +43,78 @@ function AuthForm({ route, method }: AuthFormProps) {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h1>{name}</h1>
-      <input
-        className="form-input"
-        type="text"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        placeholder="Username"
-      />
-      {method === "register" && (
-        <input
-          className="form-input"
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Email"
-        />
-      )}
-      <input
-        className="form-input"
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder="Password"
-      />
-      {loading && <p>Loading...</p>}
-      <button className="form-button" type="submit">
-        {name}
-      </button>
+    <form
+      className="flex max-w-md w-full flex-col border self-center ml-auto mr-auto rounded-sm"
+      onSubmit={handleSubmit}
+    >
+      <div className="border-b border-gray-100 p-6">
+        <h1 className="font-bold text-2xl uppercase mb-2">{name}</h1>
+        <p className="text-gray-400 uppercase text-xs mb-2 font-bold">
+          Sign in to your account
+        </p>
+      </div>
+      <div className="flex flex-col p-6 gap-5">
+        <div className="flex flex-col gap-1">
+          <label
+            className="uppercase mb-2 text-xs font-bold text-gray-400"
+            htmlFor="username"
+          >
+            Your username
+          </label>
+          <input
+            name="username"
+            className="border-2 px-2 py-2 text-sm rounded-sm placeholder:text-gray-300"
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="Username"
+          />
+        </div>
+
+        {method === "register" && (
+          <div className="flex flex-col">
+            <label
+              className="uppercase mb-2 text-xs font-bold text-gray-400"
+              htmlFor="email"
+            >
+              Your email
+            </label>
+            <input
+              className="border-2 px-2 py-2 text-sm rounded-sm placeholder:text-gray-300"
+              type="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email"
+            />
+          </div>
+        )}
+        <div className="flex flex-col">
+          <label
+            className="uppercase mb-2 text-xs font-bold text-gray-400"
+            htmlFor="password"
+          >
+            Your password
+          </label>
+          <input
+            className="border-2 px-2 py-2 text-sm rounded-sm placeholder:text-gray-300"
+            name="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+          />
+        </div>
+
+        {loading && <p>Loading...</p>}
+        <button
+          className="bg-blue-500 text-sm text-white rounded-sm py-4"
+          type="submit"
+        >
+          {name}
+        </button>
+        <button className="text-center text-gray-400">Forgot password?</button>
+      </div>
     </form>
   );
 }
