010b43d319c8fcf3f56fdf2026cb0bacb9bc133d
Marek <m.rykaczewski99@gmail.com>
2024-07-01 13:07:29 +0200

    remove typo

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -3,7 +3,7 @@ import AuthForm from "../components/AuthForm";
 const Login = () => {
   return (
     <div className="w-full h-full p-3 mt-auto mb-auto">
-      <AuthForm route="/api/token/" method="login" />;
+      <AuthForm route="/api/token/" method="login" />
     </div>
   );
 };
