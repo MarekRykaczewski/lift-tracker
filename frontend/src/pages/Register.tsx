0b5b08b618ea13eb6330ddec2ce1c46c957f3f9d
Marek <m.rykaczewski99@gmail.com>
2024-07-02 18:38:55 +0200

    register page styling

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,7 +1,11 @@
 import AuthForm from "../components/AuthForm";
 
 const Register = () => {
-  return <AuthForm route="/api/user/register/" method="register" />;
+  return (
+    <div className="w-full h-full p-3 mt-auto mb-auto">
+      <AuthForm route="/api/user/register/" method="register" />
+    </div>
+  );
 };
 
 export default Register;
