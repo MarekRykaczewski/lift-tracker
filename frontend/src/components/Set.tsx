7c8ee35ccf154618eb2a229df717e1deb2031f98
Marek <m.rykaczewski99@gmail.com>
2024-07-15 16:21:05 +0200

    ensure equal spacing between items

--- frontend/src/components/Set.tsx
+++ frontend/src/components/Set.tsx
@@ -15,16 +15,16 @@ const Set: React.FC<SetProps> = ({ set, isSelected }) => {
         isSelected ? "bg-sky-100 dark:bg-gray-700" : ""
       }`}
     >
-      <div>
-        <span className="font-bold mr-1">{set.order}</span>
+      <div className="flex-1 text-center">
+        <span className="font-bold">{set.order}</span>
       </div>
-      <div>
+      <div className="flex-1 text-center">
         <span className="font-bold mr-1">{set.display_weight}</span>
         <span className="text-gray-700 dark:text-gray-300">
           {preferredUnit}
         </span>
       </div>
-      <div>
+      <div className="flex-1 text-center">
         <span className="font-bold mr-1">{set.reps}</span>
         <span className="text-gray-700 dark:text-gray-300">reps</span>
       </div>
