1f28590c693b7a4301efe4e504e99ff7dbeaa1a9
Marek <m.rykaczewski99@gmail.com>
2024-07-15 16:23:52 +0200

    style container div

--- frontend/src/pages/SetGroupDetails.tsx
+++ frontend/src/pages/SetGroupDetails.tsx
@@ -112,7 +112,7 @@ const SetGroupDetails: React.FC = () => {
       <h1 className="w-full font-bold text-2xl text-center py-3 mb-3 dark:bg-gray-700 bg-gray-200 text-black dark:text-white border-b-4 border-sky-500">
         Track
       </h1>
-      <div className="max-w-[40vw] w-full flex flex-col">
+      <div className="max-w-[40vw] border-2 dark:border-gray-600 bg-gray-100 dark:bg-gray-700 p-2 w-full flex flex-col">
         <form
           className="flex flex-col mb-2"
           onSubmit={selectedSet ? handleUpdateSet : handleCreateSet}
