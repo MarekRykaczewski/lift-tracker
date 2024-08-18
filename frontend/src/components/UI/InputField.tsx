f4dbcafe76b267e943e02e097f67938fc40a6b2a
Marek <m.rykaczewski99@gmail.com>
2024-08-18 19:15:20 +0200

    allow number values

--- frontend/src/components/UI/InputField.tsx
+++ frontend/src/components/UI/InputField.tsx
@@ -4,7 +4,7 @@ interface InputFieldProps {
   id: string;
   label: string;
   type: string;
-  value: string;
+  value: string | number;
   onChange: React.ChangeEventHandler<HTMLInputElement>;
   placeholder: string;
 }
